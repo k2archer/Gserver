ccbf32dad22b475605edca7523fe1d725707e34c
pk.wei <pirate.kwei@gmail.com>
2018-05-07 18:34:08 +0800

    添加后台通知显示

--- app/src/main/java/pk/wei/com/gserver/BookService.java
+++ app/src/main/java/pk/wei/com/gserver/BookService.java
@@ -4,12 +4,17 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.support.v7.app.NotificationCompat;
 
 import pk.wei.com.gserver.LibraryService.ReaderServiceImpl;
 
 public class BookService extends Service {
 
     static final int READER_SERVICE_BINDER_CODE = 100;
+    static final int MANAGER_SERVICE_BINDER_CODE = 200;
 
     private IBinder iBinderPool = new IBinderPool.Stub() {
         @Override
@@ -19,10 +24,42 @@ public class BookService extends Service {
                 case READER_SERVICE_BINDER_CODE:
                     binder = new ReaderServiceImpl();
                     break;
+                case MANAGER_SERVICE_BINDER_CODE:
+//                    binder = new ManagerServiceImpl();
+                    break;
             }
             return binder;
         }
     };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotification();
+    }
+
+    public void createNotification() {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setContentTitle("图书馆管理系统")// 设置通知栏标题
+                .setContentText("运行中")         // 设置通知栏显示内容
+                .setContentIntent(getDefalutIntent(Notification.FLAG_AUTO_CANCEL)) // 设置通知栏点击意图
+                .setTicker("正在启动中...")          // 通知首次出现在通知栏，带上升动画效果的
+                .setWhen(System.currentTimeMillis()) // 通知产生的时间，会在通知信息里显示，一般是系统获取到的时间
+                .setPriority(Notification.PRIORITY_DEFAULT) // 设置该通知优先级
+                .setOngoing(true)                  // true，设置为正在进行的通知。表示后台任务
+                .setDefaults(Notification.DEFAULT_VIBRATE) // 向通知添加声音、闪灯和振动效果
+                .setSmallIcon(R.mipmap.ic_launcher);       // 设置通知小ICON
+
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
+    public PendingIntent getDefalutIntent(int flags) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, flags);
+        return pendingIntent;
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
